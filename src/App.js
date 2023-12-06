b0f05942c462d4a402217802a461e2ec806499d3
JongIn Yoon <bellpp93@gmail.com>
2023-12-07 01:03:45 +0900

    make component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
 
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
+
 class App extends Component {
   render() {
-    return <div className="App">Hello, React!!</div>;
+    return (
+      <div className="App">
+        <Subject></Subject>
+      </div>
+    );
   }
 }
 
