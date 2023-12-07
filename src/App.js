6d8687ea9ee397f856f7acef98c537133c56db98
JongIn Yoon <bellpp93@gmail.com>
2023-12-07 23:52:14 +0900

    React study - about state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,30 @@ import Subject from "./components/Subject";
 import "./App.css";
 
 class App extends Component {
+  /*
+   constructor함수가 있다면 이 함수가 가장 먼저 실행되어 초기화를 담당한다는 뜻
+   그 다음으로 초기화가 끝나면 state를 초기화 해야 한다.
+   그 다음은 state값으로 Subject컴포넌트의 props값을 설정한다.
+  */
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        // { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" },
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content
           title="HTML"
           desc="HTML is HyperText Markup Language."
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,19 +2,33 @@ import React, { Component } from "react";
 
 class TOC extends Component {
   render() {
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while (i < data.length) {
+      // 엘리먼트를 자동으로 생성하면 콘솔창(F12)에 에러가 발생한다.
+      // lists.push(
+      //   <li>
+      //     <a href={"/content/" + data[i].id}>{data[i].title}</a>
+      //   </li>
+      // );
+      /**
+       * Each child in a list should have a unique "key" prop이라는 메시지는
+       * '각 리스트 항목은 key라고 하는 prop을 가져야 한다.'라는 뜻이다.
+       * 즉, 여러 개의 항목으로 구성된 목록을 자동으로 생성할 때는
+       * 각 항목을 서로 구분할 수 있는 식별자를 지정하면 된다.
+       * 아래는 위에 코드에서 수정한 코드(<li>엘리먼트에 key={data[i].id}가 추가됨)
+       */
+      lists.push(
+        <li key={data[i].id}>
+          <a href={"/content/" + data[i].id}>{data[i].title}</a>
+        </li>
+      );
+      i = i + 1;
+    }
     return (
       <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
+        <ul>{lists}</ul>
       </nav>
     );
   }
